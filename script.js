2b743d156abf5601f9cdee6d22d371a19d940f3b
Excel Aufa Ghanivy <masexcel144@gmail.com>
2025-10-02 12:17:32 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. Pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementByld('sapaButton);
+//2. Tambahan 'event listener' untuk aksil 'click'
+tombolSapa.addEventListener('click', function() {
+  //3. jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terimakasih sudah berkunjung!');
+});
